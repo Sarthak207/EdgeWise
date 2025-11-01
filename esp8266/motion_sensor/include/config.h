7dab0003e701111bace8a6e24d40cedf4b51a439
Sarthak207 <sarthakpardeshi207@gmail.com>
2025-11-01 14:42:52 +0530

    Motion sensor func added but not tested

---
+++ esp8266/motion_sensor/include/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Wi-Fi Credentials
+#define WIFI_SSID "your_wifi_name"
+#define WIFI_PASS "your_wifi_password"
+
+// MQTT Broker (Raspberry Pi IP)
+#define MQTT_SERVER "192.168.1.xx"   // Replace with your Pi's IP
+#define MQTT_PORT 1883
+
+#endif
